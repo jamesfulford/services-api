4b8d73766299d0fe8d2c028fb5eb951b3c7361f1
James Fulford <james.fulford@outlook.com>
2021-10-27 21:28:20 -0400

    feat(src): adding postgres plumbing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './services/services.module';
 
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DatabaseConfigModule } from './database-config/database-config.module';
+import { DatabaseConfigService } from './database-config/database-config.service';
+
 @Module({
-  imports: [ServicesModule],
+  imports: [
+    ServicesModule,
+    TypeOrmModule.forRootAsync({
+      imports: [DatabaseConfigModule],
+      useFactory: (databaseConfigService: DatabaseConfigService): Promise<TypeOrmModuleOptions> => databaseConfigService.getDatabaseConfig(),
+      inject: [DatabaseConfigService]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database-config/database-config.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConfigService {
+    getDatabaseConfig(): Promise<TypeOrmModuleOptions> {
+        // (later, may read from a secrets store like Vault)
+        const host = process.env.POSTGRES_HOST || 'localhost';
+        const port = parseInt(process.env.POSTGRES_PORT, 10) || 5432;
+        const username = process.env.POSTGRES_USER || 'rootuser';
+        const password = process.env.POSTGRES_PASSWORD || 'rootpassword';
+        const database = process.env.POSTGRES_DATABASE || 'test';
+        const synchronize = (process.env.TYPEORM_SYNCHRONIZE || 'true') === 'true';
+
+        return Promise.resolve({
+            type: 'postgres',
+            host,
+            port,
+            username,
+            password,
+            database,
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            synchronize,
+        });
+    }
+}
